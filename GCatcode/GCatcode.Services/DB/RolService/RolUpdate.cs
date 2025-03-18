3a60ee3cddcea7cce592e6d2fe47bc873fdd90b9
Furiduri <jorge.furiduri@gmail.com>
2025-03-18 00:29:35 -0600

    solucion de warnig por posibles null

--- GCatcode/GCatcode.Services/DB/RolService/RolUpdate.cs
+++ GCatcode/GCatcode.Services/DB/RolService/RolUpdate.cs
@@ -8,7 +8,7 @@ namespace GCatcode.Repository.DB.RolService
         public int RolId { get; set; }
 
         [Required, MaxLength(250)]
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         [MaxLength(500)]
         public string? Description { get; set; }
